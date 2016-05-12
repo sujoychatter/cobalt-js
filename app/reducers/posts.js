5459ecdb5c6c2a82f02650f06ecff68ecc3eba0c
Sujoy Chatterjee <sujoychatts@gmail.com>
2016-05-12 11:38:25 +0530

    changes to reducer to handle case when data is already loaded

--- app/reducers/posts.js
+++ app/reducers/posts.js
@@ -5,7 +5,11 @@ const initialState = {
 export default function posts(state = initialState, action = {}) {
 	switch (action.type) {
 		case 'loadData__Post':
-			var newItems = state.items.concat(action.data.posts);
+			var newPosts = []
+			if(action.data){
+				newPosts = action.data.posts;
+			}
+			var newItems = state.items.concat(newPosts);
 			return Object.assign({}, {items: newItems});
 		default:
 			return state;
