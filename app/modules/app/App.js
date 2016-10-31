4a45e119b719742dc2de76f8512fd9ef14a7b5cd
Sujoy Chatterjee <sujoychatts@gmail.com>
2016-11-01 00:37:58 +0530

    header scss

--- app/modules/app/App.js
+++ app/modules/app/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router';
-require('../../scss/app.scss');
 
 module.exports =  React.createClass({
   render() {
--- app/modules/fixed_components/header.js
+++ app/modules/fixed_components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+require('../../scss/header.scss');
 
 var Header = React.createClass( {
   render() {
